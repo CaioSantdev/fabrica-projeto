c0275534f0445bbf74a4df8bae4b00aede67704a
Caio Santos <c.santosggwp@gmail.com>
2024-04-23 15:40:30 -0300

    modelagem da model

--- piloto/admin.py
+++ piloto/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+class StudentAdmin(admin.ModelAdmin):
+    ...
+
+class CourseAdmin(admin.ModelAdmin):
+    ...
+
+class CampunsAdmin(admin.ModelAdmin):
+    ...
+
+admin.site.register(Student,StudentAdmin)
+admin.site.register(Courses,CourseAdmin)
+admin.site.register(Campus,CampunsAdmin)
--- piloto/models.py
+++ piloto/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
 # Create your models here.
+class Campus(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Courses(models.Model):
+    name = models.CharField(max_length=100)
+    campus = models.ForeignKey(Campus, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+
+class Student(models.Model):
+    registration = models.CharField(max_length=9,unique=True)
+    name = models.CharField(max_length=100)
+    cpf = models.CharField(max_length=14,unique=True)
+    brith_date = models.DateField()
+    image = models.ImageField(upload_to='piloto/img/%Y/%m/%d/')
+    # problema de d0n_delete CASCADE -> RESOLVER!!
+    course = models.ForeignKey(Courses,on_delete=models.CASCADE)
+
+
+    VINCULADO ="Vinculado"
+    FORMADO= "Formado"
+    JUBILADO="Jubilado"
+    EVADIDO= "Evadido"
+
+    VESTIBULAR="Vestibular"
+    SISU="SISU"
+    PSENEM="PSEnem"
+
+    situacao = {
+    VINCULADO: "Vinculado",
+    FORMADO: "Formado",
+    JUBILADO: "Jubilado",
+    EVADIDO: "Evadido",
+    }
+    modo_de_entrada ={
+        VESTIBULAR: "Vestibular",
+        SISU: "SISU",
+        PSENEM: "PSEnem",
+    }
+    situation = models.CharField(max_length=10,choices=situacao,default="VINCULADO")
+    entry_mode = models.CharField(max_length=10,choices=modo_de_entrada,default="SISU")
+
+    def __str__(self):
+        return self.name
