96d355b7318dbc6e62756bca5d17deb9a5bd59bf
Caio - Ubuntu(casa) <c.santosggwp@gmail.com>
2024-05-01 19:08:01 -0300

    form filtrar

--- piloto/forms/FiltroForm.py
+++ piloto/forms/FiltroForm.py
@@ -3,8 +3,9 @@ from piloto.models import Cursos, Campus
 
 
 class FiltroForm(forms.ModelForm):
-    nomeCurso = forms.ModelChoiceField(queryset=Cursos.objects.all(),empty_label=None)
-    
+    nomeCurso = forms.ModelChoiceField(required=False,label="Nome do Curso",queryset=Cursos.objects.all(),empty_label="-----")
+    # campus = forms.ModelChoiceField(queryset=Campus.objects.all(),empty_label=None)
+    campus = forms.ModelChoiceField(required=False,queryset=Campus.objects.all(),empty_label="------")
     class Meta:
         model = Cursos
         fields = ["nomeCurso",
